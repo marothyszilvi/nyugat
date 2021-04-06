735513a427298620f027f0ebd83031ac7b6970a8
masikmisi <minko.mihaly@gmail.com>
2021-04-06 23:31:46 +0200

    html d3 files

---
+++ nyugat-datastories-2021/script.js
@@ -0,0 +1,46 @@
+
+
+// set the dimensions of the canvas
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+
+// set the ranges
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+// define the axis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom")
+
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+
+// add the SVG element
+var svg = d3.select("mydataviz").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+// load the data
+d3.json("nyugat_author_data.json", function(error, data) {
+    
+    var p = d3.select("#mydataviz").selectAll("p")
+    .data()
+    .enter()
+    .append("p");
+
+  // scale the range of the data
+  
+  
+});
